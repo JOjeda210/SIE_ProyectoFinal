ef9eb0165ffff3508d19e473336297a0c1155752
Jesús Ojeda <ojedajesus232@gmail.com>
2025-08-04 17:32:05 -0500

    Login

---
+++ src/app/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  credenciales = {
+    numeroControl: '',
+    password: ''
+  }
+
+  onLogin(): void{
+    console.log('formulario enviado', this.credenciales)
+  }
+}
